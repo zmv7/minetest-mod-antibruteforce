e2060e06c3db4bab737a6d51b4b221e6b0ccdbc4
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2022-12-28 01:08:42 +0500

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,27 @@
+local fails = {}
+
+local max_fails = core.settings:get("abf.max_auth_fails") or 3
+local wait_time = core.settings:get("abf.wait_time") or 300
+
+core.register_on_authplayer(function(name, ip, is_success)
+	if not (name and ip) then return end
+	if not is_success then
+		fails[name] = (fails[name] or 0) + 1
+		fails[ip] = (fails[ip] or 0) + 1
+	else
+		fails[name] = nil
+		fails[ip] = nil
+	end
+end)
+
+core.register_on_prejoinplayer(function(name, ip)
+	if not (name and ip) then return end
+	if (fails[name] and fails[name] > max_fails) or (fails[ip] and fails[ip] > max_fails) then
+		core.after(wait_time, function()
+			fails[name] = nil
+			fails[ip] = nil
+		end)
+		core.log("action", name.." ["..ip.."] temp-banned for too many auth fails")
+		return "Too many authentification fails. Try again after "..wait_time.." seconds."
+	end
+end)
